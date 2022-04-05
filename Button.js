2f2074663cbec52ca38fcdb588500a4386c02f6b
ThorMore1 <102993512+ThorMore1@users.noreply.github.com>
2022-04-05 14:22:21 -0400

    Add files via upload

---
+++ Button.js
@@ -0,0 +1,9 @@
+let lightning = 0;
+let outputobj = document.getElementById("div");
+document.getElementById("div").textContent = "run";
+function buttonclick( ) {
+    document.getElementById("div").textContent = "clicked";
+    lightning++;
+    element:'beans' +1 ;
+}
+ new Element= 'beans'
